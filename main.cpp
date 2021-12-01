f5b9b083b8c9949b3d0372d90056e330b258431e
ChristianLindner04 <49622992+ChristianLindner04@users.noreply.github.com>
2021-12-01 07:33:24 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,24 @@
+/* 11/30/21: Make Player class, hand class, moved some stuff from card class to hand class. Can now pull cards, asks user if they want to pull card.
+ pretty much just need to try my hand at making the game. Maybe tmrw I can make the hit/stand/whatever mechanics */
 #include <iostream>
 #include "Card.h"
+#include "Hand.h"
+#include "Player.h"
 int main(){
-    // gen card and print it
-    Card c(Card::genCardNums());
-    std::cout << Card::toString(&c) << std::endl;
-    std::cout << "TEST ONE IS DONE" << std::endl;
-    // gen deck and print it
+    srand(time(NULL));
+    // gen deck
     std::vector<Card> deck;
-    Card::genDeck(&deck);
-    Card::printDeck(&deck);
-
-
+    Hand::genDeck(&deck);
+    
+    Hand playerHand;
+    Hand cpuHand;
+    while(Player::userWantsToPlay()){
+                                                         /*PULL CARDS*/
+    playerHand.pullCard(&deck);
+    std::cout << "The card that you drew first is: " << Card::toString(playerHand.get(playerHand.getArr().size()-1)) << std::endl;
+    cpuHand.pullCard(&deck);
+    std::cout << "The card that the dealer drew first is: " << Card::toString(cpuHand.get(cpuHand.getArr().size()-1)) << std::endl;}
+    
+    
+    
 }
